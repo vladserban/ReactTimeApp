c3464a531a4f0d50cf7c7b7dad08c1fcbdb093ba
vladserban <serban.vlad@gmail.com>
2016-09-05 01:01:06 +0300

    Finished tests for Countdown

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -36,6 +36,29 @@ describe('Countdown', () => {
         done();
       }, 3001);
     });
+
+    it('should pause timer when paused', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout( () => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    {/*Min 06:48 'should reset count on stopped' doesn't require the setTimeout part. only the two assertions are sufficient.*/}
+
+    it('should reset timer when stopped', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      expect(countdown.state.count).toBe(0);
+      expect(countdown.state.countdownStatus).toBe('stopped');
+    });
   });
 
 });
