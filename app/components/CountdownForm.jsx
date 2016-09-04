ef9cabf63c0cefcf29f09ddbe34d6a132c67d280
vladserban <serban.vlad@gmail.com>
2016-09-04 23:06:02 +0300

    Tested form with a spy

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,18 +5,21 @@ var CountdownForm = React.createClass({
   handleSubmit: function(e){
     e.preventDefault();
     var totalSeconds = Math.floor(this.refs.seconds.value);
-    this.refs.seconds.value = '';
-    if(typeof totalSeconds === 'number' && totalSeconds > 0){
+
+    if (totalSeconds > 0) {
+      this.refs.seconds.value = '';
       this.props.onSetCountdown(totalSeconds);
     }
   },
 
   render: function(){
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" ref="seconds" placeholder="Enter the number of seconds" />
-        <button className="button expanded" type="submit" ref="countdownBtn">Start</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref="seconds" placeholder="Enter the number of seconds" />
+          <button className="button expanded" type="submit" ref="countdownBtn">Start</button>
+        </form>
+      </div>
     )
   }
 });
