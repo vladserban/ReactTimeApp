202dfa6fd216f542c0a96933cebdd4ed22fb47c8
vladserban <serban.vlad@gmail.com>
2016-09-04 23:58:29 +0300

    created Controls and tests

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
       Countdown: 'app/components/Countdown.jsx',
       Clock: 'app/components/Clock.jsx',
       CountdownForm: 'app/components/CountdownForm.jsx',
+      Controls: 'app/components/Controls.jsx',
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
